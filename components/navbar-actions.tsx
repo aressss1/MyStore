dae18e00270d854832f586c0977a0ba87ae939fb
aressss1 <codewithfaizi@gmail.com>
2023-07-12 10:15:23 +0500

    90%

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -3,8 +3,13 @@
 import { useEffect, useState } from "react"
 import Button from "./ui/button"
 import { ShoppingCart } from "lucide-react"
+import useCart from "@/hooks/use-cart"
+import { useRouter } from "next/navigation"
 
 const NavbarActions = () => {
+    const cart = useCart()
+    const router = useRouter()
+
     const [isMounted , setIsMounted] = useState(false)
 
     useEffect(() => {
@@ -15,15 +20,18 @@ const NavbarActions = () => {
         return null
     }
 
+
   return (
     <div className="ml-auto flex items-center gap-x-4" >
-        <Button className="flex items-center rounded-full bg-black px-4 py-2" >
+        <Button 
+            onClick={() => router.push("/cart")}
+            className="flex items-center rounded-full bg-black px-4 py-2" >
             <ShoppingCart 
                 size={20}
                 color="white"
             />
             <span className="ml-2 text-sm font-medium text-white" >
-                0
+                {cart.items.length}
             </span>
         </Button>
     </div>
--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -7,8 +7,8 @@ import { useRouter } from "next/navigation";
 
 import Currency  from "@/components/ui/currency";
 import IconButton  from "@/components/ui/icon-button";
-// import usePreviewModal from "@/hooks/use-preview-modal";
-// import useCart from "@/hooks/use-cart";
+import usePreviewModal from "@/hooks/use-preview-modal";
+import useCart from "@/hooks/use-cart";
 import { Product } from "@/types";
 
 interface ProductCard {
@@ -18,25 +18,25 @@ interface ProductCard {
 const ProductCard: React.FC<ProductCard> = ({
   data
 }) => {
-//   const previewModal = usePreviewModal();
-//   const cart = useCart();
+  const previewModal = usePreviewModal();
+  const cart = useCart();
   const router = useRouter();
 
   const handleClick = () => {
     router.push(`/product/${data?.id}`);
   };
 
-//   const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
-//     event.stopPropagation();
+  const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
 
-//     previewModal.onOpen(data);
-//   };
+    previewModal.onOpen(data);
+  };
 
-//   const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
-//     event.stopPropagation();
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
 
-//     cart.addItem(data);
-//   };
+    cart.addItem(data);
+  };
   
   return ( 
     <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
@@ -51,11 +51,11 @@ const ProductCard: React.FC<ProductCard> = ({
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
             <IconButton 
-            //   onClick={onPreview} 
+              onClick={onPreview} 
               icon={<Expand size={20} className="text-gray-600" />}
             />
             <IconButton
-            //   onClick={onAddToCart} 
+              onClick={onAddToCart} 
               icon={<ShoppingCart size={20} className="text-gray-600" />} 
             />
           </div>
