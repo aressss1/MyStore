362579bceef447fd398827bf9d5237a1dc870297
aressss1 <codewithfaizi@gmail.com>
2023-08-23 13:34:25 +0500

    fix2.0

--- components/info.tsx
+++ components/info.tsx
@@ -49,7 +49,7 @@ const Info: React.FC<InfoProps> = ({
                 </div>
             </div>
             <div className="mt-10 flex items-center gap-x-3" >
-                <Button  onClick={onAddToCart} className="flex items-center gap-x-2" >
+                <Button onClick={onAddToCart}  className="flex items-center gap-x-2" >
                     Add To Cart
                     <ShoppingCart />
                 </Button>
